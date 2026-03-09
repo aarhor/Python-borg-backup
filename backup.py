edc3ed127a0538e17634056b499f6a0dc715d0c6
aarhor <30049374+aarhor@users.noreply.github.com>
2026-03-09 21:55:28 +0100

    feat: Send Mail on error

--- backup.py
+++ backup.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import os
+from smtp import *
 
 with open(
     f"{os.path.dirname(os.path.abspath(__file__))}/config/config.json", "r"
@@ -44,10 +45,14 @@ for x in json_data["backup"]:
                     print("Backup was successful, but there were some warnings.")
                 case 2:
                     print("The Backup wasn't successful, there were a fatal error.")
+                    send_error_mail(json_data["SMTP"], Name)
         else:
             print("The repo isn't currently initialized.")
             subprocess.run(
                 ["borg", "init", "--encryption=repokey", f"{x["RemoteRepo"]}"]
+            )
+            print(
+                "The repo is now initialized. Please set the value 'Repo_Initialized' in the config to the value 'true'."
             )
     else:
         print(f"Backup '{Name}' is not active.")
---
+++ smtp.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def send_error_mail(smtpsettings, current_backup):
+    LOGINUSER = smtpsettings["Login"]
+    SENDER = smtpsettings["Sender"]
+    SMTP_SERVER = smtpsettings["SMTP_Server"]
+    SMTP_PORT = smtpsettings["Port"]
+    PASSWORD = smtpsettings["Password"]
+    RECIPIENT = smtpsettings["Recipent"]
+    MESSAGE_TEXT = "There were an error with the backup. Please see the logs for more informations."
+
+    msg = EmailMessage()
+    msg.set_content(MESSAGE_TEXT)
+    msg["Subject"] = f"Error - Borgbackup for Backup '{current_backup}'"
+    msg["From"] = SENDER
+    msg["To"] = RECIPIENT
+
+    try:
+        with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:
+            smtp.login(LOGINUSER, PASSWORD)
+            smtp.send_message(msg)
+    except Exception as e:
+        print(f"Error: {e}")
