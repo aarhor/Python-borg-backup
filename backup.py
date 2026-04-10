2b7b621621e7417f4b0b29644b7aad4f7bb70f1f
aarhor <30049374+aarhor@users.noreply.github.com>
2026-04-10 20:02:08 +0200

    Display traceback after catched exception

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import datetime
+import traceback
 from smtp import *
 from Logging import *
 from functions import *
@@ -87,6 +88,11 @@ for backup in json_data["backup"]:
         )
         MailMessage += LOG_FATAL(
             f"\t{e.args[0]}",
+            Logging_Folder_Filename,
+            LogLevel,
+        )
+        MailMessage += LOG_FATAL(
+            f"\t{traceback.format_exc()}",
             Logging_Folder_Filename,
             LogLevel,
         )
